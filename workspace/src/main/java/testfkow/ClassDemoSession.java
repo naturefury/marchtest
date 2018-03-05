1ffce40a2f8e18911964fefe4a8a8b7ca0c9fbd4
testflowmarch <naturefury@gmail.com>
2018-03-06 00:36:50 +0530

    Updated ClassDemoSession.java file

--- workspace/src/main/java/testfkow/ClassDemoSession.java
+++ workspace/src/main/java/testfkow/ClassDemoSession.java
@@ -4,15 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ClassDemoSession {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.setProperty("webdriver.chrome.driver","C:/Users/e3020651/Downloads/chromedriver.exe");		
-	WebDriver driver=new ChromeDriver();
-	driver.manage().window().maximize();
-	driver.navigate().to("https://fisglobal.fdbl.com/");
-	system.printout.ln("thankyou")
-	
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("https://fisglobal.fdbl.com/");
+		System.out.println("thankyou");
+
 	}
 
 }
